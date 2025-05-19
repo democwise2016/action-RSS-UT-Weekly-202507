c76133c62134313665366cb206724ee46096840c
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-05-20 00:50:53 +0800

    Update config-list.js

--- config-list.js
+++ config-list.js
@@ -14,7 +14,39 @@ let feedList = [
     homepageURL: 'https://www.youtube.com/channel/UC9WiXJEyHMGRqL-__3FIBEw',
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
+  {
+    title: '老张是大佬 裝 電腦',
+    feedID: 'boss_zhang',
+    homepageURL: 'https://www.youtube.com/channel/UC4p6VCir9d3dkJUu89c0m9A',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+  {
+    title: '财经不眠姐 聊 財經',
+    feedID: 'Sachajiang',
+    homepageURL: 'https://www.youtube.com/channel/UCWJ6bQDUSwRNakG4XajrqUQ',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+  {
+    title: 'IC实验室 聊 財經',
+    feedID: 'ICLAB',
+    homepageURL: 'https://www.youtube.com/channel/UCuCTKG11Ucg0XzTjODAOPXQ',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+
+  // ====== 以上 5 個 ========
   
+  {
+    title: '碎碎念工坊 玩 遊戲',
+    feedID: 'JJYY-Game',
+    homepageURL: 'https://www.youtube.com/channel/UCMrSPJyceKH7IY8Gbbw62Bw',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+  {
+    title: '3cTim哥生活日常 玩 3C',
+    feedID: '3ctim',
+    homepageURL: 'https://www.youtube.com/channel/UCmoOnxeTHUcUBWFNSUR8HvQ',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
 
   // ====== 以上 15 個，滿了！！ ========
 ]
